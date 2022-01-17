cd201330f42b4218e423105716eed6585a99719f
adamPatrick12 <adam_77paper@hotmail.com>
2022-01-16 23:50:26 -0500

    first commit

---
+++ StudyBeatsFM/src/App.jsx
@@ -0,0 +1,27 @@
+import './index.css'
+
+
+function App() {
+
+
+  return (
+    
+    <div className='container'>
+      <div className='blocker'>
+        
+      </div>
+    
+    <iframe width="1280" height="720" src="https://www.youtube.com/embed/5qap5aO4i9A?autoplay=1&mute=0" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen>
+        </iframe>
+
+    </div>
+   
+  )
+}
+
+export default App
+

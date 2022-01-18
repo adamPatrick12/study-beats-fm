fb27113d0675d6ddfc79b3a1dee103409b2e5fed
adamPatrick12 <adam_77paper@hotmail.com>
2022-01-18 13:31:43 -0500

    added audio control UI

--- StudyBeatsFM/src/components/audioControls.jsx
+++ StudyBeatsFM/src/components/audioControls.jsx
@@ -1,12 +1,41 @@
 import React from "react";
+import { GrPlayFill } from 'react-icons/gr';
+import skipBack from '../images/skipBack.png'
+import skipFoward from '../images/skipForward.png'
+import volumeOn from '../images/volumeOn.png'
+
 
 const AudioControl = () => {
     return (
         <div className="audioControlContainer">
             <div className="audioControl">
-                 audioControl
+                <div className="goBackBtn">
+                    <img className="audioImgSize" src={skipBack} alt="" />
+                </div>
+                <div className="goFowardBtn">
+                    <img src={skipFoward} className="audioImgSize" alt="" />
+                </div>
+                <div className="PlayPause">
+                    <GrPlayFill className="playBtn"></GrPlayFill>
+                </div>
+                <div className="volume">
+                    <img className="audioImgSize" src={volumeOn} alt="" />
+                </div>
+                <div >
+                    <input className="volumeDial"
+                        type="range"
+                        min={0}
+                        max={1}
+                        step={0.02}
+                        onChange={event => {
+                            setVolume(event.target.valueAsNumber)
+                                (song[songIndex].volume = volume)
+                        }}
+                    />
+                </div>
             </div>
-           
+
+
         </div>
     )
 }

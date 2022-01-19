f5c52a4c56b7afe49883c4a2140c7efc89f2e240
adamPatrick12 <adam_77paper@hotmail.com>
2022-01-18 20:09:08 -0500

    audio controls fully working

--- StudyBeatsFM/src/components/audioControls.jsx
+++ StudyBeatsFM/src/components/audioControls.jsx
@@ -7,6 +7,9 @@ import skipFoward from '../images/skipForward.png'
 import volumeOn from '../images/volumeOn.png'
 import synth from '../Music/Synthwave.mp3'
 import home from  '../Music/HOME - Resonance.mp3'
+import mute from '../images/mute.png'
+
+let lastPlayedVolume
 
 
 const AudioControl = () => {
@@ -15,15 +18,23 @@ const AudioControl = () => {
   let audio2 = new Audio(home)
   let audio3 = new Audio(synth)
 
-  const [BtnClass, setBtnClass] = useState("PlayPause")
+  const [BtnClass, setBtnClass] = useState("PlayPause")           //pause play change
   const [BtnClass2, setBtnClass2] = useState("playBtn")
+  const [playPauseImg, setPlayPause] = useState(play)
 
-  const [playPauseImg, setPlayPause] = useState(play)  //setting pause Play
+  const [muteCheck, setUnmute] = useState("volumeOn")             //unmute/mute change
+  const [muteCheck2, setUnmute2] = useState("audioOnImg")
+  const [volumeImg, setVolumeImg] = useState(volumeOn)
+  
+
+  
   const [songIndex, setSongIndex] = useState(0)
   const [song] = useState([audio, audio2, audio3])
   const [songChangeCheck, setSongChange] = useState(false)
   const [volume, setVolume] = useState(1)
-  
+  const [VolumeThumb, setVolumeThumb] = useState(volume)
+ 
+  song[songIndex].volume = volume   // Volume checker
 
     const handlePausePlaySwitch = (e) => {
         let className = e.target.className
@@ -40,6 +51,25 @@ const AudioControl = () => {
             setBtnClass("PlayPause")
             setBtnClass2("playBtn")
             pause()
+        }
+    }
+
+    const handleMute = (e) => {
+        let classNameVol = e.target.className
+        console.log(classNameVol)
+
+
+        if(classNameVol === "volumeOn" || classNameVol === "audioOnImg"){
+            setVolumeImg(mute)
+            setUnmute("volumeOff")
+            setUnmute2("audioOffImg")
+            lastPlayedVolume = volume
+            setVolume(0)
+        }else if(classNameVol === "volumeOff" || classNameVol === "audioOffImg"){
+            setVolumeImg(volumeOn)
+            setUnmute("volumeOn")
+            setUnmute2("audioOnImg")
+            setVolume(lastPlayedVolume)
         }
     }
 
@@ -92,18 +122,19 @@ useEffect(() => {
                 <div onClick={handlePausePlaySwitch} className={BtnClass}>
                   <img src={playPauseImg} className={BtnClass2} alt="" />
                 </div>
-                <div onClick={() => {console.log("On")}} className="volume">
-                    <img className="audioImgSize" src={volumeOn} alt="" />
+                <div onClick={handleMute} className={muteCheck}>
+                    <img className={muteCheck2} src={volumeImg} alt="" />
                 </div>
                 <div >
                     <input className="volumeDial"
                         type="range"
                         min={0}
                         max={1}
-                        step={0.02}
+                        value={volume}
+                        step={0.01}
                         onChange={event => {
-                            setVolume(event.target.valueAsNumber)
-                                (song[songIndex].volume = volume)
+                            setVolume(event.target.valueAsNumber)   
+                                
                         }}
                     />
                 </div>

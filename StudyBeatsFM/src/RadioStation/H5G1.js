8595a5578534f7ed907b7cc0b55be687b8f31511
adamPatrick12 <adam_77paper@hotmail.com>
2022-01-22 20:59:25 -0500

    added songs H5G1

--- StudyBeatsFM/src/RadioStation/H5G1.js
+++ StudyBeatsFM/src/RadioStation/H5G1.js
@@ -1,7 +1,37 @@
 import LakeyInspired from '../Music/H5G1/LAKEY INSPIRED - Better Days.mp3'
-
-
+import luv from '../Music/H5G1/APO ザ·犬 。 -  luvstory (full beat tape).mp3'
+ import sunset from '../Music/H5G1/APO ザ·犬 。- Sunset Ride.mp3'
+import bonita from '../Music/H5G1/Bonita.mp3'
+import blues from '../Music/H5G1/BUSHIDO BLUES.mp3'
+import seaside from '../Music/H5G1/By The Seaside (Wit U).mp3'
+import fireplace from '../Music/H5G1/Fireplace.mp3'
+import flora from '../Music/H5G1/Fora da Rotina.mp3'
+import louis from '../Music/H5G1/Gucci Louis - Rap Beat 2.mp3'
+import guru from '../Music/H5G1/Guru Griff - Ride Round.mp3'
+import jntbts from '../Music/H5G1/jntbts - dawning.mp3'
+import keva from '../Music/H5G1/kevatta - flirt.mp3'
+import pure from '../Music/H5G1/OMORI - Pure Imagination.mp3'
+import step from '../Music/H5G1/One Step Ahead.mp3'
+import beach from '../Music/H5G1/swad beach - Aruarian dance.mp3'
+import uumamii from '../Music/H5G1/uumamii- Caffè.mp3'
+ 
 export const H5G1Radio = 
-[new Audio (LakeyInspired)]
+[new Audio (LakeyInspired),
+    new Audio (luv),
+    new Audio (sunset),
+    new Audio (bonita),
+    new Audio (blues),
+    new Audio (seaside),
+    new Audio (fireplace),
+    new Audio (flora),
+    new Audio (louis),
+    new Audio (guru),
+    new Audio (jntbts),
+    new Audio (keva),
+    new Audio (pure),
+    new Audio (step),
+    new Audio (beach),
+    new Audio (uumamii),
+]
 
 export default H5G1Radio
